d290acb249916c30571837ba40a17ae06372f435
ayuubb <ayubbkj2@gmail.com>
2022-04-03 15:11:47 +0800

    Finished component parts/mostpickend + categories

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -10,7 +10,7 @@ import FormatNumber from 'utils/formatNumber';
 export default function Hero(props) {
   function showMostPick() {
     window.scrollTo({
-      top: (props.showMostPick.current.offsetTop = 30),
+      top: props.refMostPicked.current.offsetTop - 30,
       behavior: 'smooth',
     });
   }
