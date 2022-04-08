452c86b1a9de006e8db7125680290b760eeddd10
ayuubb <ayubbkj2@gmail.com>
2022-04-08 13:48:54 +0800

    add var in formDate

---
+++ src/elements/Form/InputDate/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+import propTypes from 'prop-types';
+import './index.scss';
+
+export default function Number(props) {
+  const { value, placeholder, name, min, max, prefix, suffix } = ;
+  const [inputValue, setValue] = useState(`${prefix}${value}${suffix}`);
+  return <div></div>;
+}
