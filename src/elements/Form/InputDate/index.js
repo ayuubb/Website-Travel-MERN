878e7ef2be3065e4299a8f9a28f898308306431f
ayuubb <ayubbkj2@gmail.com>
2022-04-08 13:49:19 +0800

    Update index.js

--- src/elements/Form/InputDate/index.js
+++ src/elements/Form/InputDate/index.js
@@ -4,7 +4,7 @@ import propTypes from 'prop-types';
 import './index.scss';
 
 export default function Number(props) {
-  const { value, placeholder, name, min, max, prefix, suffix } = ;
+  const { value, placeholder, name, min, max, prefix, suffix } = props;
   const [inputValue, setValue] = useState(`${prefix}${value}${suffix}`);
   return <div></div>;
 }
