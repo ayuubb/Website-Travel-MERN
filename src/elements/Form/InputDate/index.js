472343f7e814b147e2512de13a23ca603de77065
ayuubb <ayubbkj2@gmail.com>
2022-04-11 13:29:56 +0800

    Input date form

---
+++ src/elements/Form/InputDate/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import propTypes from 'prop-types';
+
+function Date(props) {
+  const { value, placeHolder, name } = props;
+  const { isShowed, setIsShowed } = useState(false);
+
+  const datePickerChange = (value) => {
+    const target = {
+      target: {
+        value: value.selection,
+        name: name,
+      },
+    };
+    props.onChange(target);
+  };
+
+  return <div>index</div>;
+}
+
+Date.propTypes = {
+  value: propTypes.object,
+  onChange: propTypes.func,
+  placeHolder: propTypes.string,
+  outerClassName: propTypes.string,
+};
+
+export default Date;
