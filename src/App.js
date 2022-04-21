afcf3e6b1798739005e97f25c027af4cfd4a06b6
ayuubb <ayubbkj2@gmail.com>
2022-04-21 13:45:16 +0800

    checkout element header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import LandingPage from 'pages/LandingPage';
 import Example from 'pages/Example';
 import DetailPage from 'pages/DetailsPage';
+import Checkout from 'pages/Checkout';
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './assets/scss/style.scss';
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Route exact path="/" component={LandingPage}></Route>
         <Route exact path="/properties/:id" component={DetailPage}></Route>
+        <Route path="/checkout" component={Checkout}></Route>
         <Route path="/example" component={Example}></Route>
       </Router>
     </div>
